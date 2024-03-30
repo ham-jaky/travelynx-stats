247c22405ab00d352160dfc6900ff7934149f344
Jakob Felix Rieckers <119401962+ham-jaky@users.noreply.github.com>
2024-03-31 00:30:44 +0100

    Gesammelte Verspätung hinzugefügt

--- main.py
+++ main.py
@@ -31,6 +31,17 @@ def get_lines(data):
 def get_first_timestamp(data):
     return datetime.datetime.fromtimestamp(data[-1]["checkin_ts"])
 
+def get_delay_arr_p_deb(data):
+    delay_sum = datetime.timedelta()
+    for ride in data:
+        rt_arr_ts = datetime.datetime.fromtimestamp(ride["rt_arr_ts"])
+        rt_dep_ts = datetime.datetime.fromtimestamp(ride["rt_dep_ts"])
+        sched_arr_ts = datetime.datetime.fromtimestamp(ride["sched_arr_ts"])
+        sched_dep_ts = datetime.datetime.fromtimestamp(ride["sched_dep_ts"])
+        delay_sum += (rt_arr_ts - sched_arr_ts)
+        delay_sum += (rt_dep_ts - sched_dep_ts)
+    return delay_sum
+
 def main():
     filename = "input-data/history.json"
     with open(filename, encoding="utf-8") as f:
@@ -40,6 +51,7 @@ def main():
     print(f"Summe der Fahrtnummern: {get_sum_of_no(data)}")
     print(f"Erste Fahrt: {get_first_timestamp(data)}")
     print(f"Gefahrene Linien: {get_lines(data)}")
+    print(f"Verspätung gesammelt (Verspätung Abfahrt + Verspätung Ankunft): {get_delay_arr_p_deb(data)}")
 
 if __name__ == "__main__":
     main()
